6e0f6540cc4c0e8a1c38292997f9cdd9c7236779
Fengli_ruomeng <72193590+Fengli-ruomeng@users.noreply.github.com>
2021-02-06 00:07:26 +0800

    Update ActBord_true.java

--- src/main/java/fengli/playerreload/nms/ActBord_true.java
+++ src/main/java/fengli/playerreload/nms/ActBord_true.java
@@ -8,12 +8,20 @@ import net.minecraft.server.v1_16_R3.*;
 public class ActBord_true implements ActBord
 {
     @Override
-    public void sendActionbar(  Player p,   String message) {
-          JSONObject json = new JSONObject();
+    public void sendActionbar(Player p,String message) {
+        JSONObject json = new JSONObject();
         json.put("text", message);
-          String logDesc = json.toString();
-          IChatBaseComponent icbc = IChatBaseComponent.ChatSerializer.a(logDesc);
-          PacketPlayOutChat bar = new PacketPlayOutChat(icbc, ChatMessageType.GAME_INFO, p.getUniqueId());
+        String logDesc = json.toString();
+        IChatBaseComponent icbc = IChatBaseComponent.ChatSerializer.a(logDesc);
+        PacketPlayOutChat bar = new PacketPlayOutChat(icbc, ChatMessageType.GAME_INFO, p.getUniqueId());
         ((CraftPlayer)p).getHandle().playerConnection.sendPacket(bar);
+    }
+    /*
+    * 尝试玩了一下NMS
+    * 此行代码目前处于BUG期
+    */
+    @Override
+    public void healPlayer(Player p,Float f){
+        ((CraftPlayer)p).getHandle().playerConnection.sendPacket(new PacketPlayOutUpdateHealth(20f,20,20));
     }
 }
