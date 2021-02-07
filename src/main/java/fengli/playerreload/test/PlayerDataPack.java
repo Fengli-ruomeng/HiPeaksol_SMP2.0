20ecb493836e2893844fff631efb9ff26e5c93f0
Fengli_ruomeng <72193590+Fengli-ruomeng@users.noreply.github.com>
2021-02-08 01:47:56 +0800

    Update PlayerDataPack.java

--- src/main/java/fengli/playerreload/test/PlayerDataPack.java
+++ src/main/java/fengli/playerreload/test/PlayerDataPack.java
@@ -22,52 +22,38 @@ public class PlayerDataPack {
     private final UUID NUUID;   private float Exp;
     //创建一个玩家的包
     public PlayerDataPack(Player player){
-        this.Health = player.getHealth();
-        this.FoodLevel = player.getFoodLevel();
-        this.Saturation = player.getSaturation();
-        this.NLocation = player.getLocation();
-        this.Name = player.getName();
-        this.NGamemode = player.getGameMode();
-        this.NUUID = player.getUniqueId();
-        this.Exp = player.getExp();
+        this.Health = player.getHealth();          this.FoodLevel = player.getFoodLevel();
+        this.Saturation = player.getSaturation();  this.NLocation = player.getLocation();
+        this.Name = player.getName();              this.NGamemode = player.getGameMode();
+        this.NUUID = player.getUniqueId();         this.Exp = player.getExp();
     }
     //自定义包,可以相当于创建了一个指定玩家的包,而数据是自定义
     //只要Name和UUID和那个玩家一致即可
     //如果是想万用包,Name和UUID请随机填入.
     public PlayerDataPack(double Health,int FoodLevel,float Saturation,Location location,String Name,GameMode gameMode,UUID UID,float Exp){
-        this.Health = Health;
-        this.FoodLevel = FoodLevel;
-        this.Saturation = Saturation;
-        this.NLocation = location;
-        this.Name = Name;
-        this.NGamemode = gameMode;
-        this.NUUID = UID;
-        this.Exp = Exp;
+        this.Health = Health;           this.FoodLevel = FoodLevel;
+        this.Saturation = Saturation;   this.NLocation = location;
+        this.Name = Name;               this.NGamemode = gameMode;
+        this.NUUID = UID;               this.Exp = Exp;
     }
     //覆盖(不覆盖关键信息，如名字和UID,万用包
     public void Covered_NoUid(Player player){
-        player.setHealth(Health);
-        player.setFoodLevel(FoodLevel);
-        player.setSaturation(Saturation);
-        player.teleport(NLocation);
-        player.setDisplayName(Name);
-        player.setGameMode(NGamemode);
+        player.setHealth(Health);          player.setFoodLevel(FoodLevel);
+        player.setSaturation(Saturation);  player.teleport(NLocation);
+        player.setDisplayName(Name);       player.setGameMode(NGamemode);
         player.setExp(Exp);
     }
     //覆盖(必须Name和UUID一致，否则返回假，其他则为真.
     public boolean Covered(Player player){
         if (player.getUniqueId().equals(NUUID)) return false;
         if (!player.getName().equals(Name)) return false;
-        player.setHealth(Health);
-        player.setFoodLevel(FoodLevel);
-        player.setSaturation(Saturation);
-        player.teleport(NLocation);
-        player.setGameMode(NGamemode);
-        player.setExp(Exp);
+        player.setHealth(Health);          player.setFoodLevel(FoodLevel);
+        player.setSaturation(Saturation);  player.teleport(NLocation);
+        player.setGameMode(NGamemode);     player.setExp(Exp);
         return true;
     }
     //不提供指定覆盖某样数据的功能，因为可以直接从某个包内提取数据然后直接Set.
-    
+
     //set
     public void setHealth(double health) { Health = health; }
     public void setFoodLevel(int foodLevel) { FoodLevel = foodLevel; }
@@ -76,7 +62,7 @@ public class PlayerDataPack {
     //Set方法里不提供设置名字和UUID 因为这两条数据是用于判断是否为特殊私有包用
     public void setGamemode(GameMode NGamemode) { this.NGamemode = NGamemode; }
     public void setExp(float exp) { Exp = exp; }
-    
+
     //get
     public double getHealth() { return Health; }
     public int getFoodLevel() { return FoodLevel; }
